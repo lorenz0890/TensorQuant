8d2d0a3998a948ed6cfa53ffa02b17b2fd1aa4f1
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-03-31 12:04:32 +0200

    getting alex net started

--- Experiments/AlexNet/main.py
+++ Experiments/AlexNet/main.py
@@ -117,7 +117,8 @@ def main():
     alexnet.fit_generator(
         train_datagen,
         steps_per_epoch= training_set.samples // batch_size,
-        validation_data = validation_set.samples // batch_size,
+        validation_set = validation_set.samples // batch_size,
+        validation_data=validation_set,
         epochs = 1,
         verbose = 1,
         callbacks=callbacks_list)
