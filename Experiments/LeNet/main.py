95d1a5a100db401455fd187cda2841f0dfece5e3
tabear18 <a11701228@unet.univie.ac.at>
2020-03-31 13:48:50 +0200

    add-bin-d4

--- Experiments/LeNet/main.py
+++ Experiments/LeNet/main.py
@@ -49,7 +49,7 @@ def main():
     #override.extr_q_map={"Conv1" : "nearest,12,11"}
     #override.weight_q_map={ "Dense4" : "nearest,16,8"}
 
-    override.weight_q_map = {"Dense3": "binary"}
+    override.weight_q_map = {"Dense4": "binary"}
 
     # QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
 
